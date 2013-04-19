b0269eb2b41872ab36f00b9a0f1f45284596f12d
Vojtech Juranek <vjuranek@redhat.com>
2013-04-19 21:29:28 +0200

    Add strategy for skipping checkout on matrix parent

--- src/main/java/org/jenkinsci/plugins/matrixcheckoustrategies/skipparent/SkipParentStrategy.java
+++ src/main/java/org/jenkinsci/plugins/matrixcheckoustrategies/skipparent/SkipParentStrategy.java
@@ -1,5 +1,50 @@
 package org.jenkinsci.plugins.matrixcheckoustrategies.skipparent;
 
-public class SkipParentStrategy {
+import hudson.Extension;
+import hudson.matrix.MatrixBuild;
+import hudson.matrix.MatrixProject;
+import hudson.model.AbstractBuild.AbstractBuildExecution;
+import hudson.model.AbstractProject;
+
+import java.io.IOException;
+
+import jenkins.scm.SCMCheckoutStrategyDescriptor;
+import jenkins.scm.SCMCheckoutStrategy;
+
+import org.kohsuke.stapler.DataBoundConstructor;
+
+public class SkipParentStrategy extends SCMCheckoutStrategy {
+    
+    @DataBoundConstructor
+    public SkipParentStrategy() {
+        
+    }
+    
+    @SuppressWarnings("rawtypes")
+    @Override
+    public void checkout(AbstractBuildExecution execution) throws IOException, InterruptedException {
+        if(execution instanceof MatrixBuild.MatrixBuildExecution) {
+            execution.getListener().getLogger().println("Skipping SCM checkout on matrix parent");
+        } else {
+            execution.defaultCheckout();
+        }
+    }
+    
+    @Extension
+    public static class SkipParentStrategyDescriptor extends SCMCheckoutStrategyDescriptor {
+        
+        @SuppressWarnings("rawtypes")
+        public boolean isApplicable(AbstractProject project) {
+            if(project instanceof MatrixProject)
+                return true;
+            return false;
+        }
+
+        public String getDisplayName() {
+            return "Skip checkout on matrix parent strategy";
+        }
+
+    }
+
 
 }
