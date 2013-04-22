97bbd6778352a292774d6771aa55f052f059bae6
Vojtech Juranek <vjuranek@redhat.com>
2013-04-22 23:51:38 +0200

    Add posibility to setup NFS location
    It's not always the case that matric parent runs on slave with NFS workspace so allow user to configure NFS location with shared sources
    explicitely. If it's not configured, workspace is assumed to be NFS volume.

--- src/main/java/org/jenkinsci/plugins/matrixcheckoustrategies/nfscopy/NFSCopyStrategy.java
+++ src/main/java/org/jenkinsci/plugins/matrixcheckoustrategies/nfscopy/NFSCopyStrategy.java
@@ -2,13 +2,17 @@ package org.jenkinsci.plugins.matrixcheckoustrategies.nfscopy;
 
 import hudson.Extension;
 import hudson.FilePath;
+import hudson.Util;
 import hudson.matrix.MatrixRun;
 import hudson.matrix.MatrixBuild;
 import hudson.matrix.MatrixProject;
+import hudson.model.AbstractBuild;
 import hudson.model.AbstractBuild.AbstractBuildExecution;
 import hudson.model.AbstractProject;
 
+import java.io.File;
 import java.io.IOException;
+import java.io.PrintStream;
 
 import jenkins.scm.SCMCheckoutStrategyDescriptor;
 import jenkins.scm.SCMCheckoutStrategy;
@@ -17,9 +21,15 @@ import org.kohsuke.stapler.DataBoundConstructor;
 
 public class NFSCopyStrategy extends SCMCheckoutStrategy {
     
+    private String nfsLocation;
+    
     @DataBoundConstructor
-    public NFSCopyStrategy() {
-        
+    public NFSCopyStrategy(String nfsLocation) {
+        this.nfsLocation = nfsLocation;
+    }
+    
+    public String getNfsLocation() {
+        return nfsLocation;
     }
     
     @SuppressWarnings("rawtypes")
@@ -28,23 +38,43 @@ public class NFSCopyStrategy extends SCMCheckoutStrategy {
         
         if(execution instanceof MatrixBuild.MatrixBuildExecution) {
             execution.defaultCheckout();
-        } else {
-            MatrixProject mp = (MatrixProject)execution.getProject().getParent();
-            FilePath ws = ((MatrixRun)execution.getBuild()).getWorkspace();
-            FilePath parentWs = ((MatrixRun)execution.getBuild()).getParentBuild().getWorkspace();
-            MatrixBuild parentBuild = ((MatrixRun)execution.getBuild()).getParentBuild();
-            
-            FilePath[] scmRoots = mp.getScm().getModuleRoots(parentWs, parentBuild);
-            for(FilePath fp : scmRoots) {
-                execution.getListener().getLogger().println("Coping " + fp.getRemote() + " to " + ws.getRemote());
-                FilePath scRoot = ws;
-                if(!"workspace".equals(fp.getBaseName())) //TODO custom workspace
-                    scRoot = new FilePath(ws, fp.getBaseName());
-                if(scRoot.exists())
-                    scRoot.deleteContents();
-                fp.copyRecursiveTo(scRoot);
-                //TODO fallback to default if NFS dir doesn't exists or there's some problem
+            if(Util.nullify(nfsLocation) != null) {
+                copySources(getScmRoots(execution), new FilePath(new File(nfsLocation)), execution.getListener().getLogger());
             }
+        } else {
+            FilePath ws = ((MatrixRun)execution.getBuild()).getWorkspace();
+            if(Util.nullify(nfsLocation) != null) {
+                FilePath[] nfsPath = {new FilePath(new File(nfsLocation))};
+                copySources(nfsPath, ws, execution.getListener().getLogger());
+            } else {
+                copySources(getScmRoots(execution), ws, execution.getListener().getLogger());
+            }
+        }
+    }
+    
+    private FilePath[] getScmRoots(AbstractBuildExecution execution) {
+        MatrixProject mp;
+        AbstractBuild mb;
+        if(execution instanceof MatrixBuild.MatrixBuildExecution) {
+            mp = (MatrixProject)execution.getProject();
+            mb = (MatrixBuild)execution.getBuild();
+        } else {
+            mp = (MatrixProject)execution.getProject().getParent();
+            mb = ((MatrixRun)execution.getBuild()).getParentBuild();
+        } 
+        return mp.getScm().getModuleRoots(mb.getWorkspace(), mb);
+    }
+    
+    private void copySources(FilePath[] scmRoots, FilePath ws, PrintStream logger) throws IOException, InterruptedException {
+        for(FilePath fp : scmRoots) {
+            logger.println("Coping " + fp.getRemote() + " to " + ws.getRemote());
+            FilePath scRoot = ws;
+            if(!"workspace".equals(fp.getBaseName())) //TODO custom workspace
+                scRoot = new FilePath(ws, fp.getBaseName());
+            if(scRoot.exists())
+                scRoot.deleteContents();
+            fp.copyRecursiveTo(scRoot);
+            //TODO fallback to default if NFS dir doesn't exists or there's some problem
         }
     }
     
